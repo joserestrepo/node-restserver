36cede789f08020a02a566931f1fa2327bb7b23f
joserestrepo <joserestrepo301@gmail.com>
2020-02-02 17:46:10 -0500

    Agregando variable de entorno db

--- server/config/config.js
+++ server/config/config.js
@@ -5,12 +5,4 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //url base de datos
-let urlDB;
-
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe';
-} else {
-    urlDB = 'mongodb+srv://admin:rRXLKmuhRne0ysP7@cluster0-pgadm.mongodb.net/cafe';
-}
-
-process.env.URL_DB = urlDB;
+process.env.URL_DB = process.env.MONGO_URI || 'mongodb://localhost:27017/cafe';
